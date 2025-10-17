9c31ef3ffc50c1909511f76194d1e31c27aad790
Bruno Cavichia <bcavichia@hiberus.com>
2025-10-17 11:51:06 +0200

    refactor: deleted aspnet boilerplate code

--- toDoApp.ApiService/Program.cs
+++ toDoApp.ApiService/Program.cs
@@ -49,37 +49,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = new[]
-{
-    "Freezing",
-    "Bracing",
-    "Chilly",
-    "Cool",
-    "Mild",
-    "Warm",
-    "Balmy",
-    "Hot",
-    "Sweltering",
-    "Scorching",
-};
-
-app.MapGet(
-        "/weatherforecast",
-        () =>
-        {
-            var forecast = Enumerable
-                .Range(1, 5)
-                .Select(index => new WeatherForecast(
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-                .ToArray();
-            return forecast;
-        }
-    )
-    .WithName("GetWeatherForecast");
-
 app.MapDefaultEndpoints();
 app.MapControllers();
 
